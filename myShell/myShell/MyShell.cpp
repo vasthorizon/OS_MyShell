57a5c2853704175152b64b3f9390b62cf77bc987
YoungHa Kim <vasthorizon@nhnnext.org>
2014-08-11 23:48:05 +0900

    과제 5
    myShell start, echo 구현

--- myShell/myShell/MyShell.cpp
+++ myShell/myShell/MyShell.cpp
@@ -13,23 +13,33 @@
 #define STR_LEN			256
 #define CMD_TOKEN_NUM	10
 
+// GLOBAL VARIABLES
 TCHAR ERROR_CMD[] 
  = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+TCHAR cmdString[STR_LEN];
+TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
+TCHAR seps[] = _T(" ,\t\n");
 
-int CmdProcessing(void);
+
+int CmdProcessing(int);
 TCHAR * StrLower(TCHAR *);
 void ShowFilesInDirectory(TCHAR *filePath);
+int CmdReadTokenize(void);
 
-int main(int agrc, TCHAR * argv[])
+
+int main(int argc, TCHAR * argv[])
 {
-	
 	// 한글 입력을 가능케 하기 위해.
-	_tsetlocale(LC_ALL, _T("Korean")); 
-	
+	_tsetlocale(LC_ALL, _T("Korean"));
+		
 	DWORD isExit;
 	while(1)
 	{
-		isExit = CmdProcessing();
+		//int tokenNum = CmdReadTokenize();
+		//if (tokenNum == 0) //Enter 입력시 처리를 위해...
+		//	continue;
+
+		isExit = CmdProcessing(0);
 		if(isExit == TRUE)
 		{
 			_fputts(_T("명령어 처리를 종료합니다. \n"), stdout );
@@ -41,13 +51,10 @@ int main(int agrc, TCHAR * argv[])
 }
 
 
-TCHAR cmdString[STR_LEN];
-TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
-TCHAR seps[]   = _T(" ,\t\n");
 
 
 
-int CmdProcessing(void)
+int CmdProcessing(int tokenNum)
 {
 	_fputts( _T("Best command prompt(type 'h' for help(>> "), stdout );
 	_getts(cmdString);
@@ -57,8 +64,12 @@ int CmdProcessing(void)
 	TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
 	TCHAR optString[STR_LEN] = { 0, };
 
+	BOOL isRun;
 
-	int tokenNum = 0;
+	STARTUPINFO si = { 0, };
+	PROCESS_INFORMATION pi;
+	si.cb = sizeof(si);
+		
 	while(token != NULL)
 	{
 		_tcscpy(
@@ -70,6 +81,30 @@ int CmdProcessing(void)
 	if( !_tcscmp(cmdTokenList[0],_T("exit")) )
 	{
 		return TRUE;
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("start")))
+	{
+		if (tokenNum >1)
+		{
+			for (int i = 1; i<tokenNum; i++)
+				_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+			_stprintf(cmdStringWithOptions, _T("%s %s"), _T("myShell.exe"), optString);
+		}
+		else
+			_stprintf(cmdStringWithOptions, _T("%s"), _T("myShell.exe"));
+
+		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
+	{
+		for (int i = 1; i<tokenNum; i++)
+			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+
+		_tprintf(_T("echo message:%s \n"), optString);
 	}
 	else if (!_tcscmp(cmdTokenList[0], _T("pwd")))
 	{
@@ -101,14 +136,10 @@ int CmdProcessing(void)
 		|| !_tcscmp(cmdTokenList[0], _T("h"))
 		)
 	{
-		printf_s("\n<commands> \nexplorer c:\\  \ndir \ndir .. \ndir C:\\ \nhelp \npwd \nexit \n\n");
+		printf_s("\n<commands>\necho \nexplorer c:\\  \ndir \ndir .. \ndir C:\\ \nhelp \npwd \nexit \nstart \n\n");
 	}
 	else
 	{	
-		BOOL isRun;
-		STARTUPINFO si = { 0, };
-		PROCESS_INFORMATION pi;
-		si.cb = sizeof(si);
 
 		_tcscpy(cmdStringWithOptions, cmdTokenList[0]);
 
@@ -127,44 +158,24 @@ int CmdProcessing(void)
 	return 0;
 }
 
-int CmdProcessing(int tokenNum)
+int CmdReadTokenize(void)
 {
-	BOOL isRun;
-	STARTUPINFO si = { 0, };
-	PROCESS_INFORMATION pi;
+	TCHAR * token;
 
-	TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
-	TCHAR optString[STR_LEN] = { 0, };
+	_fputts(_T("Best command prompt>> "), stdout);
+	_getts(cmdString);
 
-	si.cb = sizeof(si);
-	if (!_tcscmp(cmdTokenList[0], _T("exit")))
+	token = _tcstok(cmdString, seps);
+
+	int tokenNum = 0;
+
+	while (token != NULL)
 	{
-		return TRUE;
-	}
-	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
-	{
-		for (int i = 1; i<tokenNum; i++)
-			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
-
-		_tprintf(_T("echo message:%s \n"), optString);
-	}
-	else
-	{
-		_tcscpy(cmdStringWithOptions, cmdTokenList[0]);
-
-		for (int i = 1; i<tokenNum; i++)
-			_stprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
-
-		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
-
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
-
-		if (isRun == FALSE)
-			_tprintf(ERROR_CMD, cmdTokenList[0]);
+		_tcscpy(cmdTokenList[tokenNum++], StrLower(token));
+		token = _tcstok(NULL, seps);
 	}
 
-	return 0;
+	return tokenNum;
 }
 
 
