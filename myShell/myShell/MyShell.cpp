289fe84c82217198c0f9be9b5cb8d8c95d4f91c8
YoungHa Kim <vasthorizon@nhnnext.org>
2014-08-12 00:33:09 +0900

    과제 5
    인자가 깨져서 넘어오는 버그 수정

--- myShell/myShell/MyShell.cpp
+++ myShell/myShell/MyShell.cpp
@@ -1,84 +1,165 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_NON_CONFORMING_SWPRINTFS
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <tchar.h>
 #include <locale.h>
 #include <windows.h> 
+#include <tlhelp32.h>
 #include <string>
-#include <vector>
-#include <iostream>
 
-#define STR_LEN			256
-#define CMD_TOKEN_NUM	10
+#define STR_LEN    256
+#define CMD_TOKEN_NUM  10
 
-// GLOBAL VARIABLES
-TCHAR ERROR_CMD[] 
- = _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+TCHAR ERROR_CMD[]
+= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+
 TCHAR cmdString[STR_LEN];
 TCHAR cmdTokenList[CMD_TOKEN_NUM][STR_LEN];
 TCHAR seps[] = _T(" ,\t\n");
 
-
+int CmdReadTokenize(void);
 int CmdProcessing(int);
 TCHAR * StrLower(TCHAR *);
 void ShowFilesInDirectory(TCHAR *filePath);
-int CmdReadTokenize(void);
 
-
-int main(int argc, TCHAR * argv[])
+int _tmain(int argc, TCHAR * argv[])
 {
 	// 한글 입력을 가능케 하기 위해.
 	_tsetlocale(LC_ALL, _T("Korean"));
-		
-	DWORD isExit;
-	while(1)
-	{
-		//int tokenNum = CmdReadTokenize();
-		//if (tokenNum == 0) //Enter 입력시 처리를 위해...
-		//	continue;
 
-		isExit = CmdProcessing(0);
-		if(isExit == TRUE)
+	if (argc > 2)  // 매개변수 전달인자가 있는 경우.
+	{
+		for (int i = 1; i<argc; i++)
+			_tcscpy_s(cmdTokenList[i - 1], argv[i]);
+
+		CmdProcessing(argc - 1);
+	}
+
+	DWORD isExit = NULL;
+	while (1)
+	{
+		int tokenNum = CmdReadTokenize();
+		if (tokenNum == 0) //Enter 입력시 처리를 위해...
+			continue;
+
+		isExit = CmdProcessing(tokenNum);
+		if (isExit == TRUE)
 		{
-			_fputts(_T("명령어 처리를 종료합니다. \n"), stdout );
+			_fputts(_T("명령어 처리를 종료합니다. \n"), stdout);
 			break;
 		}
 	}
 
-	return 0; 
+	return 0;
 }
 
+int CmdReadTokenize(void)
+{
+	TCHAR * token;
+	TCHAR * nextToken;
 
+	_fputts(_T("Best command prompt(type 'h' for help(>> "), stdout);
+	_getts_s(cmdString);
 
+	token = _tcstok_s(cmdString, seps, &nextToken);
 
+	int tokenNum = 0;
+
+	while (token != NULL)
+	{
+		_tcscpy_s(cmdTokenList[tokenNum++], StrLower(token));
+		token = _tcstok_s(NULL, seps, &nextToken);
+	}
+
+	return tokenNum;
+}
+
+void ListProcessInfo(void)
+{
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolhelp32Snapshot error! \n"));
+		return;
+	}
+
+	PROCESSENTRY32 pe32;	/* 프로세스 정보 얻기위한 구조체 변수 */
+
+	/* PROCESSENTRY32 변수는 사용하기 전에 크기 정보 초기화 해야한다 */
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	/* 첫 번째 프로세스 정보 얻음 */
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First error! \n"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+
+	do
+	{
+		/* 프로세스 이름, ID 정보 출력 */
+		_tprintf(_T("%25s %5d \n"), pe32.szExeFile, pe32.th32ProcessID);
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+}
+
+void KillProcess(void)
+{
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(_T("CreateToolhelp32Snapshot (of processes)"));
+		return;
+	}
+
+	PROCESSENTRY32 pe32;
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		_tprintf(_T("Process32First"));
+		CloseHandle(hProcessSnap);
+		return;
+	}
+
+	HANDLE hProcess;
+	BOOL isKill = FALSE;
+	do
+	{
+		if (_tcscmp(pe32.szExeFile, cmdTokenList[1]) == 0)
+		{
+			hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+
+			if (hProcess != NULL)
+			{
+				TerminateProcess(hProcess, -1);
+				isKill = TRUE;
+			}
+
+			CloseHandle(hProcess);
+			break;
+		}
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+
+	if (isKill == FALSE)
+		_tprintf(_T("Kill process fail, Try again! \n"));
+}
 
 int CmdProcessing(int tokenNum)
 {
-	_fputts( _T("Best command prompt(type 'h' for help(>> "), stdout );
-	_getts(cmdString);
-
-	TCHAR * token = _tcstok(cmdString, seps);
+	BOOL isRun;
+	STARTUPINFO si = { 0, };
+	PROCESS_INFORMATION pi;
 
 	TCHAR cmdStringWithOptions[STR_LEN] = { 0, };
 	TCHAR optString[STR_LEN] = { 0, };
 
-	BOOL isRun;
-
-	STARTUPINFO si = { 0, };
-	PROCESS_INFORMATION pi;
 	si.cb = sizeof(si);
-		
-	while(token != NULL)
-	{
-		_tcscpy(
-			cmdTokenList[tokenNum++], StrLower(token)
-		);
-		token = _tcstok(NULL, seps);
-	}
-	
-	if( !_tcscmp(cmdTokenList[0],_T("exit")) )
+	if (!_tcscmp(cmdTokenList[0], _T("exit")))
 	{
 		return TRUE;
 	}
@@ -87,12 +168,12 @@ int CmdProcessing(int tokenNum)
 		if (tokenNum >1)
 		{
 			for (int i = 1; i<tokenNum; i++)
-				_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+				_stprintf_s(optString, _T("%s %s"), optString, cmdTokenList[i]);
 
-			_stprintf(cmdStringWithOptions, _T("%s %s"), _T("myShell.exe"), optString);
+			_stprintf_s(cmdStringWithOptions, _T("%s %s"), _T("MyShell.exe"), optString);
 		}
 		else
-			_stprintf(cmdStringWithOptions, _T("%s"), _T("myShell.exe"));
+			_stprintf_s(cmdStringWithOptions, _T("%s"), _T("MyShell.exe"));
 
 		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
 
@@ -102,7 +183,7 @@ int CmdProcessing(int tokenNum)
 	else if (!_tcscmp(cmdTokenList[0], _T("echo")))
 	{
 		for (int i = 1; i<tokenNum; i++)
-			_stprintf(optString, _T("%s %s"), optString, cmdTokenList[i]);
+			_stprintf_s(optString, _T("%s %s"), optString, cmdTokenList[i]);
 
 		_tprintf(_T("echo message:%s \n"), optString);
 	}
@@ -110,7 +191,7 @@ int CmdProcessing(int tokenNum)
 	{
 		_fputts(_T("Input password to show current directory:>"), stdout);
 		TCHAR inputStr[STR_LEN];
-		_getts(inputStr);
+		_getts_s(inputStr);
 
 		if (!_tcscmp(inputStr, _T("mypwd")))
 		{
@@ -122,7 +203,7 @@ int CmdProcessing(int tokenNum)
 		}
 
 	}
-	else if ( !_tcscmp(cmdTokenList[0],_T("dir")) )
+	else if (!_tcscmp(cmdTokenList[0], _T("dir")))
 	{
 		int paramLen = _tcslen(cmdTokenList[1]);
 
@@ -131,7 +212,7 @@ int CmdProcessing(int tokenNum)
 		else
 			ShowFilesInDirectory(cmdTokenList[1]);
 	}
-	else if ( !_tcscmp(cmdTokenList[0],_T("help"))
+	else if (!_tcscmp(cmdTokenList[0], _T("help"))
 		|| !_tcscmp(cmdTokenList[0], _T("?"))
 		|| !_tcscmp(cmdTokenList[0], _T("h"))
 		)
@@ -139,12 +220,11 @@ int CmdProcessing(int tokenNum)
 		printf_s("\n<commands>\necho \nexplorer c:\\  \ndir \ndir .. \ndir C:\\ \nhelp \npwd \nexit \nstart \n\n");
 	}
 	else
-	{	
-
-		_tcscpy(cmdStringWithOptions, cmdTokenList[0]);
+	{
+		_tcscpy_s(cmdStringWithOptions, cmdTokenList[0]);
 
 		for (int i = 1; i<tokenNum; i++)
-			_swprintf(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
+			_stprintf_s(cmdStringWithOptions, _T("%s %s"), cmdStringWithOptions, cmdTokenList[i]);
 
 		isRun = CreateProcess(NULL, cmdStringWithOptions, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
 
@@ -158,35 +238,16 @@ int CmdProcessing(int tokenNum)
 	return 0;
 }
 
-int CmdReadTokenize(void)
-{
-	TCHAR * token;
-
-	_fputts(_T("Best command prompt>> "), stdout);
-	_getts(cmdString);
-
-	token = _tcstok(cmdString, seps);
-
-	int tokenNum = 0;
-
-	while (token != NULL)
-	{
-		_tcscpy(cmdTokenList[tokenNum++], StrLower(token));
-		token = _tcstok(NULL, seps);
-	}
-
-	return tokenNum;
-}
-
 
 TCHAR * StrLower(TCHAR *pStr)
 {
 	TCHAR *ret = pStr;
 
-	while(*pStr)
+	while (*pStr)
 	{
-		if(_istupper(*pStr))
+		if (_istupper(*pStr))
 			*pStr = _totlower(*pStr);
+
 		pStr++;
 	}
 
@@ -196,11 +257,11 @@ TCHAR * StrLower(TCHAR *pStr)
 void ShowFilesInDirectory(TCHAR *filePath)
 {
 	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind;	
-	
+	HANDLE hFind;
+
 	std::wstring targetStr = filePath;
 	targetStr += _T("\\*.*");
-	
+
 	hFind = FindFirstFile(targetStr.c_str(), &FindFileData);
 	if (hFind == INVALID_HANDLE_VALUE)
 	{
@@ -216,7 +277,7 @@ void ShowFilesInDirectory(TCHAR *filePath)
 
 	} while (FindNextFile(hFind, &FindFileData));
 	printf_s("\n");
-		
+
 	FindClose(hFind);
-	
+
 }
